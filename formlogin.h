3620649b5e44fcf0f4cbb8312c31ddca0c1aae0f
Henry <csthenry@foxmail.com>
2021-12-21 21:06:50 +0800

    更新版权信息。

--- formlogin.h
+++ formlogin.h
@@ -1,3 +1,11 @@
+/***************************************************/
+/*              Magic Light Assistant              */
+/* Copyright (c) 2017-2021 by bytecho.net          */
+/* Written by Henry                                */
+/* Function:                                       */
+/* Communication, activity, management and approval*/
+/***************************************************/
+
 #ifndef FORMLOGIN_H
 #define FORMLOGIN_H
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/***************************************************/
+/*              Magic Light Assistant              */
+/* Copyright (c) 2017-2021 by bytecho.net          */
+/* Written by Henry                                */
+/* Function:                                       */
+/* Communication, activity, management and approval*/
+/***************************************************/
+
 #include "mainwindow.h"
 #include "formlogin.h"
 #include <QApplication>
