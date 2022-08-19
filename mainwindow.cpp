1936c520c05d2eca7eff90cde4fdd0c587c283ca
Henry <csthenry@foxmail.com>
2022-08-19 17:48:05 +0800

    优化UI，更新HarmonyOS字体。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -450,6 +450,19 @@ MainWindow::MainWindow(QWidget *parent, QDialog *formLoginWindow)
             }
         }
     });
+
+    //更新HarmonyOS字体
+    QFont font;
+    int size, font_Id = QFontDatabase::addApplicationFont(":/src/font/HarmonyOS_Sans_SC_Regular.ttf");
+    QStringList fontName = QFontDatabase::applicationFontFamilies(font_Id);
+    font.setFamily(fontName.at(0));
+    auto listWidget = findChildren<QWidget*>();
+    for (auto& widget : listWidget) //遍历所有组件
+    {
+        size = widget->font().pointSize();
+        font.setPointSize(size);
+        widget->setFont(font);
+    }
 }
 MainWindow::~MainWindow()
 {
